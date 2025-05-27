02cd0f36f4c26575189af134895d120bce5d0020
Serhii Vladimirov <vladimirovsk@gmail.com>
2025-05-27 10:55:47 -0600

    disable cases

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Container} from '@mui/material';
 import Services from '@/app/(site)/services/page';
 import Skills from '@/app/(site)/skills/page';
 import Advantages from '@/app/(site)/advantages/page';
-import Cases from '@/app/(site)/cases/page';
+// import Cases from '@/app/(site)/cases/page';
 import Contact from '@/app/(site)/contact/page';
 
 export default function Home() {
