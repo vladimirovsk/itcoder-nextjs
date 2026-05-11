0f8cc7d330675338ef8a1d4518d93b91a92904b8
Serhii Vladimirov <vladimirovsk@gmail.com>
2026-05-11 16:16:02 -0600

    removed FAQPage

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,5 +10,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 1,
     },
+    {
+      url: 'https://itcoder.ca/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ];
 }
